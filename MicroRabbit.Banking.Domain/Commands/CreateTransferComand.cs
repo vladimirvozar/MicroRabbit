4931f540eac355b53229e4f284b071d6349d5ffd
Vladimir Vozar <vladimirvozar@gmail.com>
2020-05-18 18:23:07 +0200

    Implemented Publish and SendCommand for Banking Microservice

---
+++ MicroRabbit.Banking.Domain/Commands/CreateTransferComand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Commands
+{
+    public class CreateTransferComand : TransferCommand
+    {
+        public CreateTransferComand(int from, int to, decimal amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
